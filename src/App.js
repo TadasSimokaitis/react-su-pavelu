d0a0e800c6081bb0cb3c5cfbbf887c823c305bde
Tadas Simokaitis <tadassimokaitis@yahoo.com>
2022-06-21 18:20:50 +0300

    added form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-// import logo from './logo.svg';
-import './App.css';
-import Header from './Header';
+
+import "./App.css";
+import Header from "./Header";
+import { useState } from "react";
+import UserForm from "./UserForm";
 
 function App() {
+  const [parentCount, setParentCount] = useState(0);
+  function addOneToCount() {
+    setParentCount(parentCount + 1);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        < Header />
+        <h1>Total: {parentCount}</h1>
+        <Header title={"First counter"} onButtonClick={addOneToCount} />
+        <Header title={"Second counter"} onButtonClick={addOneToCount} />
       </header>
+      <UserForm />
     </div>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,41 +1,34 @@
-import { useState } from "react"
+import { useState } from "react";
+import Total from "./TotalCounts";
 
-export default function Header(){
-    
-    const [state, setState] = useState(0)
-    const [count, setCount] = useState(0)
-    
-    const style = {
-        color: 'white',
-        fontSize: 20,
-        margin: '5px',
-        borderRadius: '5px',
-        background:'black'
-      };
-      
-    return(
-        <div>
-        <p >How many times{count}</p>
-        <p >Clicked {state} times</p>
-        <button style={style} onClick={() => {setState(state + 1); setCount(count +1)} }>
-          Plius
-        </button>
-        <button style={style} onClick={() => {setState(state - 1); setCount(count +1)}}>
-          Minus
-        </button>
-        <button style={style}onClick={() => {setState(state * 2); setCount(count +1)}}>
-          Daugyba is 2
-        </button>
-        <button style={style}onClick={() => {setState(state / 2); setCount(count +1)}}>
-          Dalyba is 2
-        </button>
-        <button style={style}onClick={() => {setState(state * 0); setCount(count +1)}}>
-          nunulint
-        </button>
+export default function Header(props) {
+  const { title, onButtonClick } = props;
+  const [count, setCount] = useState(0);
+  const [buttonPressed, setButtonPressed] = useState(0);
 
-
-        
-        
-      </div>
-    )
+  return (
+    <div>
+      <h3>{title}</h3>
+      <button
+        onClick={() => {
+          setCount(count -1);
+          setButtonPressed(buttonPressed + 1);
+          onButtonClick();
+        }}
+      >
+        -
+      </button>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          setButtonPressed(buttonPressed + 1);
+          onButtonClick();
+        }}
+      >
+        +
+      </button>
+      <h4>{count}</h4>
+      <Total  buttonPressed={buttonPressed} />
+    </div>
+  );
 }
